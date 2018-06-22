4bffd6fe84fc5022668260caaaea4c1ddc9672ad
LuciaVG <bvluly@gmail.com>
2018-06-22 00:39:11 -0500

    methods and interfaces

---
+++ go-tour/error.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number:  %g", e)
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+	prev := 0.0
+	z := 1.0
+	for math.Abs(z-prev) > .005 {
+		fmt.Println(z)
+		prev = z
+		z -= (z*z - x) / (2 * z)
+	}
+	return z, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
