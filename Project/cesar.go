e0672d2f4a986bde4a1c252946a32aee7b6472f4
LuciaVG <bvluly@gmail.com>
2018-06-27 04:00:05 -0500

    added decription

--- Project/cesar.go
+++ Project/cesar.go
@@ -8,20 +8,15 @@ import (
 	"os"
 )
 
-func main() {
-
-	public_key := 7.0
-	private_key_a := 14.0
-	private_key_b := 16.0
-
-	key := public_key * private_key_a * private_key_b
+func decrypt(key float64, msg_file string) (float64, string) {
 	sft := 0.0
+	sfd := 0.0
 	fmt.Println(key)
 
 	zero := 33.0
 	md := 94.0
 
-	file, err := os.Open("file.txt")
+	file, err := os.Open("file2.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -34,7 +29,56 @@ func main() {
 	text2 := make([]byte, len(text))
 
 	for i, chr := range text {
-		fmt.Println(sft)
+		switch {
+		case chr > 32:
+			if chr == byte(zero+sft) {
+				text2[i] = 32
+			} else {
+				sft = math.Mod(key, md)
+
+				if sft == 0 {
+					key++
+					sft = math.Mod(key, md)
+				}
+				sfd = float64(chr) - (sft + 0)
+				if sfd > (zero - 1) {
+					text2[i] = byte(sfd)
+				} else {
+					text2[i] = byte(md + sfd)
+				}
+				key += math.Floor(key / sft)
+			}
+
+		case chr == 32:
+			text2[i] = byte(zero + sft)
+		default:
+			text2[i] = chr
+		}
+	}
+
+	fmt.Print(text2)
+	return key, string(text2)
+}
+
+func encrypt(key float64, msg_file string) (float64, string) {
+	sft := 0.0
+	zero := 33.0
+	md := 94.0
+
+	file, err := os.Open(msg_file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	text, err := ioutil.ReadAll(file)
+	fmt.Print(text)
+	fmt.Print(string(text))
+
+	text2 := make([]byte, len(text))
+
+	for i, chr := range text {
+		//fmt.Println(sft)
 		switch {
 		case chr > 32:
 			sft = math.Mod(key, md)
@@ -56,5 +100,37 @@ func main() {
 	}
 
 	fmt.Print(text2)
-	fmt.Print(string(text2))
+	return key, string(text2)
+}
+
+func main() {
+	if len(os.Args) < 4 {
+		fmt.Println("Arguments missing")
+		return
+	}
+	public_key := 10.0
+	private_key_a := 14.0
+	private_key_b := 16.0
+
+	key := public_key * private_key_a * private_key_b
+	c := ""
+	switch os.Args[1] {
+	case "en":
+		_, c = encrypt(key, os.Args[2])
+
+	case "de":
+		_, c = decrypt(key, os.Args[2])
+	default:
+		fmt.Println("Need a valid argument")
+	}
+	fmt.Println(c)
+
+	file, err := os.Create(os.Args[3])
+	if err != nil {
+		log.Fatal("Cannot create file", err)
+	}
+	defer file.Close()
+
+	fmt.Fprintf(file, c)
+
 }
